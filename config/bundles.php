224d4dafa7d0622cdd5144c9510c0de6a57e8cb2
Marco Ferrari <marco.msg.ferrari@gmail.com>
2018-02-02 19:09:21 +0100

    easyadmin added

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,6 @@ return [
     Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
